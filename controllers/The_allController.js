2e60d51abdc8e8440632811a2e46c3ceff7f465e
abdenacer <nasseressaouira@gmail.com>
2022-10-06 15:02:39 +0800

    affichage articles et categories visteur

--- controllers/The_allController.js
+++ controllers/The_allController.js
@@ -4,15 +4,25 @@ const { where } = require('sequelize');
 const db = require('../models');
 
 // Create Main Model
+const Article = db.articles
+const Category = db.categorys
 
 // Get All
 const getAll = async(req , res) => {
-    res.render('The-all');
+    const data = {};
+    Article.findAll().then(e =>{
+        data.article = e;
+        Category.findAll().then(e =>{
+            data.category = e;
+        })
+        .then(() => { res.render('The-all', { data })})
+        .catch(() => { res.send('Error') })
+    })
 }
 
 // Get One
 const getone = async (req, res) => {
-    res.render('The-all');
+    res.render('The-all')
 }
 
 
